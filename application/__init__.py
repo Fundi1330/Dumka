8f3f26809adcb7595ca9ac7db0b9cf858e3cd5e5
I_am_jumper_My_job_is__to_jump <dimadereza1@gmail.com>
2023-03-11 13:37:27 +0200

    added dacorators

---
+++ application/__init__.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+app.config['SELECT_KEY'] = '1234'
+
+@app.route('/') #Головна сторінка
+def golovna():
+    form = 'Це головна сторінка'
+    return render_template('index.html', form=form)     #тут Христині треба html файл зробить
+
+
+@app.route('/sing_in') #Реєстрація
+def sing_in():
+    return render_template('sing_in.html')  #тут Христині теж треба html файл зробить
+
+@app.route('/log_in') #вхід на акк
+def log_in():
+    return render_template('log_in.html')  #тут Христині треба html файл зробить
+
+@app.route('/public')
+def public():
+    return render_template('public.html')  # тут Христині треба html файл зробить
