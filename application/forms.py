89bcaed39e3df79ae9ac38c737f8160a5d7626d9
numb8r <mixa4719@gmail.com>
2023-03-12 19:46:42 +0100

    EditForm and EditFormprivat

---
+++ application/forms.py
@@ -0,0 +1,34 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Email
+from wtforms.validators import Length
+from hashlib import md5
+
+
+class SingUp(FlaskForm):  # Для реєстрації, треба html
+    name = StringField('Name', validators=[DataRequired()])
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    remember_me = BooleanField('Remember Me!')
+
+
+class Login(FlaskForm):  # Для входу, треба html
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+
+
+class EditForm(FlaskForm):  # Для того щоб миняти імя и тд треба бд
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('about_me', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Submit')  # Button
+
+    def avatar(self, size):
+        return """Силка на фото""" + md5(self.email).hexdigest() + '?d=mm&s=' + str(size)
+
+
+class EditFormprivat(FlaskForm):  # Теж треба бд
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Submit')  # Button
