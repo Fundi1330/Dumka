e82e7f21e2874aa789518cc85fb911dc17b20ac6
I_am_jumper_My_job_is__to_jump <dimadereza1@gmail.com>
2023-03-13 11:58:49 +0200

    new import for login

--- application/__init__.py
+++ application/__init__.py
@@ -1,27 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for
+from .forms import SingUp, Login
+from flask_login import current_user, LoginManager
 
 app = Flask(__name__)
 
 app.config['SELECT_KEY'] = '1234'
 
+
+login = LoginManager(app)
+
 @app.route('/') #Головна сторінка
 @app.route('/index')
 def main():
     form = 'Це головна сторінка'
-    return render_template('index.html', form=form)     #тут Христині треба html файл зробить
+    return render_template('index.html', form=form)     # треба html файл зробить
 
 
 @app.route('/sing_in') #Реєстрація
 def sing_in():
-    return render_template('sing_in.html')  #тут Христині теж треба html файл зробить
+    return render_template('sing_in.html')  # теж треба html файл зробить
 
-@app.route('/log_in') #вхід на акк
+@app.route('/log_in', methods=['GET', 'POST']) #вхід на акк
 def log_in():
-    return render_template('log_in.html')  #тут Христині треба html файл зробить
+    return render_template('log_in.html')  #треба html файл зробить
 
 @app.route('/public') #публікація
 def public():
-    return render_template('public.html')  # тут Христині треба html файл зробить
+    return render_template('public.html')  #треба html файл зробить
 
 @app.route('/user/<username>') #декоратор для перегляду профілю
 def user(username):
