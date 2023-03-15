02c741ef3a22270b79a4eb2263c785782bd9c3bb
Illia <ilakaru70@gmail.com>
2023-03-15 19:14:37 +0200

    updated models.py

--- application/models.py
+++ application/models.py
@@ -1,18 +1,24 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+from sqlalchemy.dialects.postgresql import ARRAY
 
 db = SQLAlchemy()
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     __tablename__ = 'User'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, index=True)
     username = db.Column(db.String(120), index=True, unique=True)
     email = db.Column(db.String(60), index=True, unique=True)
     password_hash = db.Column(db.String(120))
-    about_me = db.Column(db.string)
+    about_me = db.Column(db.String)
 
-
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
     def __repr__(self) -> str:
         return '<User {}'.format(self.username)
 
@@ -20,7 +26,7 @@ class Post(db.Model):
     __tablename__ = 'Post'
     id = db.Column(db.Integer, primary_key=True)
     theme = db.Column(db.String)
-    tag = db.Column(db.String, index=True)
+    tags = db.Column, ARRAY(db.String, index=True)
     author = db.Column(db.String, index=True)
 
 
