9202f0f059839c1e3a1b8ec9ca398f53fe19cfaf
Fundi1330 <88210377+Fundi1330@users.noreply.github.com>
2023-03-16 18:40:35 +0200

    fixed __init__.py and base.html

--- application/__init__.py
+++ application/__init__.py
@@ -1,43 +1,39 @@
 from flask import Flask, render_template, flash, redirect, url_for
 from application.forms import Registration, Login
-from flask_login import current_user, LoginManager, login_user
+from flask_login import current_user, LoginManager, login_user, logout_user
 from application.models import User, Post, Community, db
-from .forms import Registration, Login
-from flask_login import current_user, LoginManager, login_user
-from .models import User, Post, Community, db
 from flask_migrate import Migrate
-from .forms import Posts as PostForm
-
+from application.forms import Posts as PostForm
+from flask_admin import Admin
+from flask_admin.contrib.fileadmin import FileAdmin
+import os.path as op
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = 'StandWithUkraine'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:12345678990@localhost:5432/alya_redit'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+path = op.join(op.dirname(__file__), 'static')
 
 db.init_app(app)
 migrate = Migrate(app, db)
 
 login = LoginManager(app)
 
-<<<<<<< HEAD
-@app.before_first_request
-def create_tables():
-=======
-
-
 admin = Admin(app, "OP", url='/admin')       #поки без паролю
 
 
 with app.app_context():
->>>>>>> 0fcc290 (corrected name of func)
     db.create_all()
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
 
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect('index')
 
 @app.route('/', methods=['GET', 'POST']) #Головна сторінка
-@app.route('/index', methods=['GET', 'POST'])
+@app.route('/index/', methods=['GET', 'POST'])
 def main():
     posts = Post.query.all()
     recomended_communities = Community.query.all()
@@ -49,13 +45,8 @@ def main():
     return render_template('index.html', title='Reddit', posts=posts, recomended_communities=recomended_communities, form=form)
 
 
-<<<<<<< HEAD
-@app.route('/sing_in', methods=['GET', 'POST']) #Реєстрація
-def sing_up():
-=======
 @app.route('/singup', methods=['GET', 'POST']) #Реєстрація
 def signup():
->>>>>>> 0fcc290 (corrected name of func)
     form = Registration()
     if form.validate_on_submit():
         user = User(name=form.name.data, username=form.username.data, email=form.email.data, password_hash=form.password.data)
@@ -65,23 +56,18 @@ def signup():
         return redirect(url_for('log_in'))
     return render_template('authorization/register.html', title='Sing Up', form=form)
 
-<<<<<<< HEAD
-@app.route('/log_in', methods=['GET', 'POST']) #вхід на акк
-def log_in():
-=======
 @app.route('/login', methods=['GET', 'POST']) #вхід на акк
 def login():
->>>>>>> 0fcc290 (corrected name of func)
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+       return redirect('index')
     form = Login()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
-            flash('Invalid username or password')
-            return redirect(url_for('log_in'))
+            flash("Incorrect login data: check password or username")
+            return redirect(url_for("login"))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
+        return redirect('index')
     return render_template('authorization/login.html', title='Log In', form=form)
 
 @app.route('/public') #публікація
@@ -90,8 +76,10 @@ def public():
 
 @app.route('/user/<username>') #декоратор для перегляду профілю
 def user(username):
-    return render_template('user.html', title=username)
+    return render_template('users/profile.html', title=username)
 
 @app.route('/kind/<subreddit>') #Сабреддіт
 def kind(subreddit):
     return render_template('kind.html', title='subreddit')
+
+admin.add_view(FileAdmin(path, '/static/', name='files'))
