58792ba2c6732bbe7f88403236b2c3d857fc427b
Illia <ilakaru70@gmail.com>
2023-03-17 16:28:49 +0200

    updated models.py

--- application/models.py
+++ application/models.py
@@ -1,6 +1,11 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
+<<<<<<< HEAD
+=======
+from sqlalchemy.dialects.postgresql import ARRAY
+from datetime import datetime
+>>>>>>> 9ec4e77 (updated models.py)
 
 db = SQLAlchemy()
 
@@ -12,7 +17,8 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(120), index=True, unique=True)
     email = db.Column(db.String(60), index=True, unique=True)
     password_hash = db.Column(db.String(120))
-    about_me = db.Column(db.String)
+    about_me = db.Column(db.String, index=True)
+    interests = db.Column(db.String, index=True)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -24,9 +30,17 @@ class User(db.Model, UserMixin):
 class Post(db.Model):
     __tablename__ = 'Post'
     id = db.Column(db.Integer, primary_key=True)
+<<<<<<< HEAD
     theme = db.Column(db.String)
     tags = db.Column(db.String, index=True)
+=======
+    theme = db.Column(db.String(60))
+    text = db.Column(db.String(140))
+    date_of_publication = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    tags = db.Column(ARRAY(db.String))
+>>>>>>> 9ec4e77 (updated models.py)
     author = db.Column(db.String, index=True)
+    likes = db.Column(db.Integer, index=True)
 
 
     def __repr__(self) -> str:
@@ -40,4 +54,8 @@ class Community(db.Model):
 
 
     def __repr__(self) -> str:
+<<<<<<< HEAD
         return '<Community {}'.format(self.name)
+=======
+        return '<Community{}'.format(self.name)
+>>>>>>> 9ec4e77 (updated models.py)
