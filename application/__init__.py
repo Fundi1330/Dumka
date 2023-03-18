7c5b5cdda71e2216472677188861b8071a89f5b5
I_am_jumper_My_job_is__to_jump <dimadereza1@gmail.com>
2023-03-18 12:13:39 +0200

    asd

--- application/__init__.py
+++ application/__init__.py
@@ -8,6 +8,8 @@ from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 from flask_admin.contrib.fileadmin import FileAdmin
 import os.path as op
+import datetime
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'StandWithUkraine'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:dima20+20@localhost:5432/alya_redit'
@@ -36,14 +38,15 @@ def logout():
 @app.route('/', methods=['GET', 'POST']) #Головна сторінка
 @app.route('/index/', methods=['GET', 'POST'])
 def main():
-    posts = Post.query.order_by(date_of_publication).all()
+    posts = Post.query.order_by(Post.date_of_publication).all()
     recomended_communities = Community.query.all()
     form = PostForm()
     if form.validate_on_submit() and current_user.is_authenticated:
         tags = '{' + str(form.tag.data) + '}'
-        post = Post(theme=form.title.data, tags=tags, author=current_user.username)
+        post = Post(theme=form.title.data, tags=tags, author=current_user.username, text=form.posts.data, likes=0, date_of_publication=datetime.datetime.now())
         db.session.add(post)
         db.session.commit()
+        flash('Пост успішно доданий на сайт!')
     return render_template('index.html', title='Reddit', posts=posts, recomended_communities=recomended_communities, form=form)
 
 
@@ -84,21 +87,9 @@ def user(username):
 def kind(subreddit):
     return render_template('kind.html', title='subreddit')
 
-
 @app.route('/post/<post>')
 def post(post):
     return render_template('post.html', title='Post')
-
-@app.route('/faq')
-def faq():
-    return render_template('footer/faq.html', title='Faq')
-
-@app.route('/aboutus')
-def about_us():
-    return render_template('footer/about_us.html', title="About us")
-
-
-
 
 admin.add_view(FileAdmin(path, '/static/', name='files'))
 admin.add_view(ModelView(User, db.session, name='Користувачі'))
