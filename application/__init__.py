850dc4324047b61d2d2973e114d9001e7a656baa
I_am_jumper_My_job_is__to_jump <dimadereza1@gmail.com>
2023-03-20 17:49:26 +0200

    added url_for, category(in flash) and rename title

--- application/__init__.py
+++ application/__init__.py
@@ -9,6 +9,7 @@ from flask_admin.contrib.sqla import ModelView
 from flask_admin.contrib.fileadmin import FileAdmin
 import os.path as op
 import datetime
+from re import findall
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'StandWithUkraine'
@@ -42,15 +43,24 @@ def main():
     recomended_communities = Community.query.all()
     form = PostForm()
     if form.validate_on_submit() and current_user.is_authenticated:
-        tags = '{' + str(form.tag.data) + '}'
+        # tag_list = form.tag.data.split(', ')
+        # flash(tag_list)
+        # tags = ' '.join(tag_list)
+        # flash(tags)
+        # changed_tags = tags.replace('[', '{').replace(']', '}').replace('\'', '\"').replace('\" \"', '')
+        # flash(changed_tags)
+        tags = findall('[a-z]{1,}', form.tag.data.lower())
+        flash(tags)
+        tags = findall('[a-z]{1,}|[a-z]{1,}\s[a-z]{1,}', form.tag.data.lower())
         post = Post(theme=form.title.data, tags=tags, author=current_user.username, text=form.posts.data, likes=0, date_of_publication=datetime.datetime.now())
         db.session.add(post)
         db.session.commit()
-        flash('Пост успішно доданий на сайт!')
+        flash('Пост успішно доданий на сайт!', category='succes')
+        return redirect('index')
+    elif not current_user.is_authenticated:
+        flash('Спочатку вам потрібно зареєструватися!', category='error')
 
-
-
-    return render_template('index.html', title='Reddit', posts=posts, recomended_communities=recomended_communities, form=form)
+    return render_template('index.html', title='Реддіти', posts=posts, recomended_communities=recomended_communities, form=form)
 
 
 @app.route('/singup', methods=['GET', 'POST']) #Реєстрація
@@ -61,9 +71,8 @@ def signup():
         user.set_password(form.password.data)
         db.session.add(user)
         db.session.commit()
-        return redirect(url_for('log_in'))
-
-    return render_template('authorization/register.html', title='Sing Up', form=form)
+        return redirect(url_for('login'))
+    return render_template('authorization/register.html', title='Реєстрація', form=form)
 
 @app.route('/login', methods=['GET', 'POST']) #вхід на акк
 def login():
@@ -73,15 +82,15 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
-            flash("Incorrect login data: check password or username")
+            flash("Неправильний логін або пароль", category='error')
             return redirect(url_for("login"))
         login_user(user, remember=form.remember_me.data)
         return redirect('index')
-    return render_template('authorization/login.html', title='Log In', form=form)
+    return render_template('authorization/login.html', title='Вхід', form=form)
 
 @app.route('/public') #публікація
 def public():
-    return render_template('public.html', title='Public your reddit')
+    return render_template('public.html', title='Публікуй свій реддіт')
 
 @app.route('/user/<username>') #декоратор для перегляду профілю
 def user(username):
@@ -89,14 +98,13 @@ def user(username):
 
 @app.route('/kind/<subreddit>') #Сабреддіт
 def kind(subreddit):
-    return render_template('kind.html', title='subreddit')
+    return render_template('kind.html', title='Сабреддіт')
 
-@app.route('/editpost/<int:id>')
-def edit_post(id):
-    post = Post.query.filter_by(id=id).first()
-    return render_template('edit_post', title='Edit your post')
+@app.route('/editpost')
+def edit_post():
+    return render_template('edit_post.html', title='Зміни свій пост')
 
-@app.route('/post/<int:id>')
+@app.route('/post/<int:id>', methods=['GET', 'POST'])
 def post(id):
     post = Post.query.filter_by(id=id).first()
 
@@ -105,14 +113,10 @@ def post(id):
         db.session.commit()
         return redirect('/')
     elif 'edit' in request.form:
-        return redirect('/editpost')
+        return redirect(url_for('/editpost', id=post.id))
 
-    return render_template('post.html', title='Post')
+    return render_template('posts/post.html', title='Пост', post=post)
 
 
 admin.add_view(FileAdmin(path, '/static/', name='files'))
 admin.add_view(ModelView(User, db.session, name='Користувачі'))
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
