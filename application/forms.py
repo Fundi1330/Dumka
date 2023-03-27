7aa546bc0cb6fde5eca71a226ea95a33e87954fc
numb8r <mixa4719@gmail.com>
2023-03-27 10:13:13 +0200

    - EditCommunity and + photo

--- application/forms.py
+++ application/forms.py
@@ -68,14 +68,7 @@ class Community(FlaskForm):
                                              DataRequired('Не може бути пусте')])
     description = TextAreaField('Опис', validators=[Length(min=0, max=500, message='За над то багато символив'),
                                            DataRequired('Не може бути пусте')])
-    submit = SubmitField('Підтвердити')
+    photo = FileField('!')  # ! == Назва файла
 
-
-class EditCommunity(FlaskForm):
-    name = TextAreaField('Користувач', validators=[DataRequired('Не може бути пусте')])
-    tema = TextAreaField('Тема', validators=[Length(min=0, max=400, message='За над то багато символив'),
-                                             DataRequired('Не може бути пусте')])
-    description = TextAreaField('Опис', validators=[Length(min=0, max=500, message='За над то багато символив'),
-                                           DataRequired('Не може бути пусте')])
     submit = SubmitField('Підтвердити')
 
